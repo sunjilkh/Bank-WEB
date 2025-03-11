90447cd10081f9a1b514b327512adc6767976987
sunjilkh <sunzilkhandaker@gmail.com>
2025-03-12 01:40:19 +0600

    second commit

--- script.js
+++ script.js
@@ -29,10 +29,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
 ///////////////////////////////////
 ///////////////////////////////////
 ///////////////////////////////////
 
+
+//Introducing DOM
+/*
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -66,3 +70,69 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+//Styles, Attributes
+message.style.backgroundColor ='#37383d';  
+message.style.width = '140%';
+
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).height);
+message.style.height = Number.parseFloat(getComputedStyle(message).height)+ 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orange')
+
+//Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+//not standard
+console.log(logo.tester);
+console.log(logo.getAttribute('tester'));
+logo.setAttribute('company', 'Bankist');
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+//Data Attributes
+console.log(logo.dataset.versionNumber);
+
+//Classes
+logo.classList.add('cin','jonson');
+logo.classList.remove();
+logo.classList.toggle();
+logo.classList.contains();
+*/
+
+const btnScrolTo  = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+// console.log(section1.getBoundingClientRect());
+btnScrolTo.addEventListener(
+  'click', function(e){
+    // const scrl = section1.getBoundingClientRect();
+    // console.log(e.target.getBoundingClientRect());
+    // console.log(
+    //   'height/width',
+    //   document.documentElement.clientHeight,
+    //   document.documentElement.clientWidth
+    // );
+    section1.scrollIntoView({behavior: 'smooth'});
+  }
+)
+
+//Adding event listner
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', function(e){
+//   alert('hello');
+
+//   // h1.removeEventListener('mouseenter');
+//   //or
+//   setTimeout(function(){
+//     h1.removeEventListener('mouseenter');
+//   }, 2000)
+//   }
+// )
